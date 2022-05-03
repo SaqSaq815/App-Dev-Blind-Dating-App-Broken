5a66234dd628c09b742d1ec5c74d7515dbfe0bf0
jgrospe92 <jeffreygrospe@yahoo.com>
2022-05-02 23:03:16 -0400

    Added Class
    Added User Class
    Added Helper Class

---
+++ AppDevTeamProject/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AppDevTeamProject
+{
+    class User
+    {
+
+        // Unique user Identifier
+        private int ID { set; get; }
+
+        // User Info to fill in
+        private string FirstName { set; get; }
+        private string LastName { set; get; }
+        private int Age { set; get; }
+        private int Location { set; get; }
+        private string Bio { set; get; }
+        private string[] Attributes { set; get; }
+
+        // User logins
+        private string Username { set; get; }
+        private int Password { set; get; }
+
+
+        // User preferences
+        private string[] Interest { set; get; }
+
+
+        // Constructor
+        public User(int iD, string firstName, string lastName, int age, int location, string bio, string[] attributes, string username, int password, string[] interest)
+        {
+            ID = iD;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Location = location;
+            Bio = bio;
+            Attributes = attributes;
+            Username = username;
+            Password = password;
+            Interest = interest;
+
+        }
+
+    }
+}
